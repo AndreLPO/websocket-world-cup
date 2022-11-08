cf409cf11b13d701e040e2d19e5b24f36b64cea6
André Oliveira <andrelpo573@gmail.com>
2022-11-08 17:49:07 -0300

    push to github

--- main.py
+++ main.py
@@ -2,16 +2,7 @@ import asyncio
 import websockets
 import json
 
-matches = []
 groups = ["A", "B", "C", "D", "E", "F", "G", "H"]
-
-
-# async def echo(websocket):
-#     async for message in websocket:
-#         port = 6000
-#         jsonMsg = json.loads(message)
-#         await websocket.send('{"return": "Mensagem recebida na porta %s"}' % (port))
-#         print('Porta do trabalhador:', jsonMsg['msg1'])
 
 
 async def worldCupPredict(websocket):
@@ -24,14 +15,19 @@ async def groupMatches(teams, groupIndex, websocket):
     await websocket.send('Grupo %s' % groups[groupIndex])
     teamsInGroup = list(
         filter(lambda t: t['grupo'] == groups[groupIndex], teams))
-    await resolveMatch(teamsInGroup, websocket)
+    await resolveGroup(teamsInGroup, websocket)
     if (groupIndex < len(groups) - 1):
         await groupMatches(teams, groupIndex + 1, websocket)
 
 
-async def resolveMatch(groupTeams, websocket):
+async def resolveGroup(groupTeams, websocket):
     # TODO: aplicar funcionalidade e lógica a função
     await websocket.send(json.dumps(groupTeams))
+
+
+async def playRounds(matchTeams, round, playedMatches, websocket):
+    # TODO: aplicar funcionalidade e lógica a função
+    print(matchTeams)
 
 
 async def main():
