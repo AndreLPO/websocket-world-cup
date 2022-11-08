65f2a7123b7d411c3eac7833e1edd341f92964c3
André Oliveira <andrelpo573@gmail.com>
2022-11-07 22:03:02 -0300

    used python filter function to select teams in ever group, and created resolve match method

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ async def worldCupPredict(websocket):
 
 async def groupMatches(teams, groupIndex, websocket):
     await websocket.send('Grupo %s' % groups[groupIndex])
-    for team in teams:
-        if (team['grupo'] == groups[groupIndex]):
-            await websocket.send(team['time'])
+    teamsInGroup = list(
+        filter(lambda t: t['grupo'] == groups[groupIndex], teams))
+    await resolveMatch(teamsInGroup, websocket)
     if (groupIndex < len(groups) - 1):
-        await websocket.send('-'*8)
         await groupMatches(teams, groupIndex + 1, websocket)
 
 
-async def resolveMatch():
-    print('match')
+async def resolveMatch(groupTeams, websocket):
+    # TODO: aplicar funcionalidade e lógica a função
+    await websocket.send(json.dumps(groupTeams))
 
 
 async def main():
