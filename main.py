6482a6aab2675edbf60e6344773db7ab1e11a0a9
André Oliveira <andrelpo573@gmail.com>
2022-11-09 13:50:06 -0300

    adicionada recursividade na faseDeGrupos e traduzido os nomes para português, porque somos brasileiros e é isso

--- main.py
+++ main.py
@@ -4,65 +4,78 @@ import random
 
 import websockets
 
-groups = ["A", "B", "C", "D", "E", "F", "G", "H"]
+grupos = ["A", "B", "C", "D", "E", "F", "G", "H"]
 # Bla
 
 
-async def worldCupPredict(websocket):
+async def previsaoDaCopa(websocket):
     async for message in websocket:
-        allGroups = json.loads(message)
-        await groupMatches(allGroups, 0, websocket)
+        todosOsGrupos = json.loads(message)
+        await partidasPorGrupo(todosOsGrupos, 0, websocket)
 
 
-async def groupMatches(teams, groupIndex, websocket):
-    await websocket.send('Grupo %s' % groups[groupIndex])
-    teamsInGroup = list(
-        filter(lambda t: t['grupo'] == groups[groupIndex], teams))
-    await resolveGroup(teamsInGroup, [], websocket)
-    if (groupIndex < len(groups) - 1):
-        await groupMatches(teams, groupIndex + 1, websocket)
+async def partidasPorGrupo(teams, groupIndex, websocket):
+    timesNoGrupo = list(
+        filter(lambda t: t['grupo'] == grupos[groupIndex], teams))
+    await faseDeGrupos(grupos[groupIndex], timesNoGrupo, [], websocket)
+    if (groupIndex < len(grupos) - 1):
+        await partidasPorGrupo(teams, groupIndex + 1, websocket)
 
 
-async def resolveGroup(groupTeams, playedMatches, websocket):
-    print("Rodada 1")
+async def faseDeGrupos(grupo, timesDoGrupo, partidasJogadas, websocket):
+    rodada = len(partidasJogadas) + 1
+    await websocket.send(f"Rodada {rodada}")
+    partida = 1
+    resultadoDaRodada = {
+        "grupo": grupo,
+        "resultados": []
+    }
     for i in range(0, 4, 2):
         x = i
-        print("Partida %s" % str(i + 1))
-        matchResult = await playRounds(groupTeams[x:x+2])
-        print(matchResult)
-        
-    await websocket.send(json.dumps(groupTeams))
+        print("Partida %s" % str(partida))
+        resultadoDaPartida = await jogarPartida(timesDoGrupo[x:x+2])
+        resultadoDaRodada["resultados"].append(resultadoDaPartida)
+        partida += 1
+    print(resultadoDaRodada)
+    await websocket.send(json.dumps(resultadoDaRodada))
+    partidasJogadas.append(resultadoDaRodada["resultados"])
+    if (rodada < 3):
+        await faseDeGrupos(grupo, timesDoGrupo, partidasJogadas, websocket)
 
 
-async def playRounds(matchTeams):
-    gameResults = []
-    for j in matchTeams:
-        teamGoal = (j['forca'] / 10) * random.randint(0,6)
-        gameResults.append({
+def verificaSeOsTimesJaJogaram(times, partidasJogadas):
+    print("bla")
+
+
+async def jogarPartida(timesDaPartida):
+    resultadosDosJogos = []
+    for j in timesDaPartida:
+        golsDoTime = (j['forca'] / 10) * random.randint(0, 6)
+        resultadosDosJogos.append({
             "time": j['time'],
-            "gols": int(teamGoal),
-            "points": 0
-            })
-    
-    if (gameResults[0]['gols'] > gameResults[1]['gols']):
-        gameResults[0]['points'] = 3
-    
-    if (gameResults[0]['gols'] == gameResults[1]['gols']):
-        gameResults[0]['points'] = 1
+            "gols": int(golsDoTime),
+            "pontos": 0
+        })
 
-    if (gameResults[1]['gols'] > gameResults[0]['gols']):
-        gameResults[1]['points'] = 3
-    
-    if (gameResults[1]['gols'] == gameResults[0]['gols']):
-        gameResults[1]['points'] = 1
+    if (resultadosDosJogos[0]['gols'] > resultadosDosJogos[1]['gols']):
+        resultadosDosJogos[0]['pontos'] = 3
 
-    return gameResults
+    if (resultadosDosJogos[0]['gols'] == resultadosDosJogos[1]['gols']):
+        resultadosDosJogos[0]['pontos'] = 1
+
+    if (resultadosDosJogos[1]['gols'] > resultadosDosJogos[0]['gols']):
+        resultadosDosJogos[1]['pontos'] = 3
+
+    if (resultadosDosJogos[1]['gols'] == resultadosDosJogos[0]['gols']):
+        resultadosDosJogos[1]['pontos'] = 1
+
+    return resultadosDosJogos
 
 
 async def main():
     port = 6000
-    async with websockets.serve(worldCupPredict, "localhost", port):
-        print('Esperando pedidos...')
+    async with websockets.serve(previsaoDaCopa, "localhost", port):
+        print(f'Servidor iniciado na porta {port}, aguardando times')
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
