58b0dc2541812a503bb7cc1824a71e0224e75fe7
André Oliveira <andrelpo573@gmail.com>
2022-11-10 22:11:43 -0300

    tentativa de implementar pontos fifas no calculo para definir a quantidade de gols, tentativa utilizando media ponderada, alterar esse metodo

--- dockerfile
+++ dockerfile
@@ -1,5 +1,6 @@
 FROM python:latest
 ADD main.py /
 RUN pip install websockets
+RUN pip install pandas
 EXPOSE 6000
 CMD ["python", "./main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 import random
-
+import pandas
 import websockets
 
 grupos = ["A", "B", "C", "D", "E", "F", "G", "H"]
@@ -37,26 +37,44 @@ async def faseDeGrupos(grupo, timesDoGrupo, partidasJogadas, websocket):
         resultadoDaRodada["resultados"].append(resultadoDaPartida)
         partida += 1
     # print(resultadoDaRodada)
-    await websocket.send(json.dumps(resultadoDaRodada))
+    await websocket.send(json.dumps(resultadoDaRodada, ensure_ascii=False))
     partidasJogadas.append(resultadoDaRodada["resultados"])
     timesOrdenadosParaNovaRodada = defineAsPartidasDaRodada(
         timesDoGrupo, rodada)
-    if(rodada == 3): classificacaoFinalDaFaseDeGrupos(grupo, partidasJogadas)
+    if (rodada == 3):
+        await websocket.send(f"Fim da fase de grupos! Resultados do Grupo: {grupo}")
+        await classificacaoFinalDaFaseDeGrupos(grupo, partidasJogadas, websocket)
 
     if (rodada < 3):
         await faseDeGrupos(grupo, timesOrdenadosParaNovaRodada, partidasJogadas, websocket)
 
-def classificacaoFinalDaFaseDeGrupos(grupo, partidas):
+
+def defineChaveParaOrdenacaoDaClassificacao(e):
+    return e['pontos']
+
+
+async def classificacaoFinalDaFaseDeGrupos(grupo, partidas, websocket):
     # TODO https://stackoverflow.com/questions/47055259/python-dict-group-and-sum-multiple-values
-    print({"grupo": grupo, "classificação": partidas})
+    classificacao = []
+    for p in partidas:
+        for t in p:
+            for c in t:
+                classificacao.append(c)
+
+    df = pandas.DataFrame(classificacao)
+    g = df.groupby('time', as_index=False).sum()
+    d = g.to_dict('records')
+    d.sort(
+        reverse=True, key=defineChaveParaOrdenacaoDaClassificacao)
+    await websocket.send(json.dumps({"grupo": grupo, "classificacao": d, "classificados": d[0:2]}, ensure_ascii=False))
 
 
-def defineChaveParaOrdenacao(e):
+def defineChaveParaOrdenacaoDasRodadas(e):
     return e['time']
 
 
 def defineAsPartidasDaRodada(times, rodada):
-    times.sort(key=defineChaveParaOrdenacao)
+    times.sort(key=defineChaveParaOrdenacaoDasRodadas)
     ordem = [0, 1, 2, 3]
     if rodada == 2:
         ordem = [0, 2, 1, 3]
@@ -68,7 +86,8 @@ def defineAsPartidasDaRodada(times, rodada):
 async def jogarPartida(timesDaPartida):
     resultadosDaPartidaPorTime = []
     for j in timesDaPartida:
-        golsDoTime = (j['forca'] / 10) * random.randint(0, 6)
+        golsDoTime = ((j['forca']*3) + (j['fifa']*2) + (random.randint(0, 6))) / \
+            (j['forca'] + 3 + j['fifa'] + 2 + random.randint(0, 6) + 1)
         resultadosDaPartidaPorTime.append({
             "time": j['time'],
             "gols": int(golsDoTime),
